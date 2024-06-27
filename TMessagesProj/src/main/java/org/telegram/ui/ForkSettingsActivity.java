d469a1f09dbd5c43350a60090b3f701897474517
r3pr3ss10n <ivegotonlyonelife@proton.me>
2024-06-27 06:49:50 -0700

    refactor & cleanup advanced settings

--- TMessagesProj/src/main/java/org/telegram/ui/ForkSettingsActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/ForkSettingsActivity.java
@@ -1,7 +1,3 @@
-/*
- * Copyright 23rd, 2019.
- */
-
 package org.telegram.ui;
 
 import android.content.Context;
@@ -23,15 +19,13 @@ import org.telegram.ui.ActionBar.ActionBar;
 import org.telegram.ui.ActionBar.BaseFragment;
 import org.telegram.ui.ActionBar.Theme;
 import org.telegram.ui.ActionBar.ThemeDescription;
-import org.telegram.ui.Cells.EmptyCell;
 import org.telegram.ui.Cells.HeaderCell;
-import org.telegram.ui.Cells.NotificationsCheckCell;
 import org.telegram.ui.Cells.ShadowSectionCell;
 import org.telegram.ui.Cells.TextCheckCell;
-import org.telegram.ui.Cells.TextDetailSettingsCell;
-import org.telegram.ui.Cells.TextSettingsCell;
 import org.telegram.ui.Components.LayoutHelper;
 import org.telegram.ui.Components.RecyclerListView;
+
+import java.util.ArrayList;
 
 public class ForkSettingsActivity extends BaseFragment {
 
@@ -40,28 +34,34 @@ public class ForkSettingsActivity extends BaseFragment {
 
     private int rowCount;
     private int sectionRow1;
-
     private int inappCameraRow;
-
-    private int emptyRow;
 
     @Override
     public boolean onFragmentCreate() {
         super.onFragmentCreate();
-
         rowCount = 0;
-        
         sectionRow1 = rowCount++;
         inappCameraRow = rowCount++;
-
         return true;
     }
 
     @Override
     public View createView(Context context) {
+        setupActionBar(context);
+        listAdapter = new ListAdapter(context);
+
+        FrameLayout frameLayout = new FrameLayout(context);
+        frameLayout.setBackgroundColor(Theme.getColor(Theme.key_windowBackgroundGray));
+        fragmentView = frameLayout;
+
+        setupRecyclerView(context, frameLayout);
+
+        return fragmentView;
+    }
+
+    private void setupActionBar(Context context) {
         actionBar.setBackButtonImage(R.drawable.ic_ab_back);
         actionBar.setTitle(LocaleController.getString("ForkSettingsTitle", R.string.ForkSettingsTitle));
-
         if (AndroidUtilities.isTablet()) {
             actionBar.setOccupyStatusBar(false);
         }
@@ -73,36 +73,25 @@ public class ForkSettingsActivity extends BaseFragment {
                 }
             }
         });
+    }
 
-        listAdapter = new ListAdapter(context);
-
-        fragmentView = new FrameLayout(context);
-        fragmentView.setBackgroundColor(Theme.getColor(Theme.key_windowBackgroundGray));
-        FrameLayout frameLayout = (FrameLayout) fragmentView;
-
+    private void setupRecyclerView(Context context, FrameLayout frameLayout) {
         listView = new RecyclerListView(context);
         listView.setVerticalScrollBarEnabled(false);
-        listView.setLayoutManager(new LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false) {
-            @Override
-            public boolean supportsPredictiveItemAnimations() {
-                return false;
-            }
-        });
+        listView.setLayoutManager(new LinearLayoutManager(context));
         listView.setGlowColor(Theme.getColor(Theme.key_avatar_backgroundActionBarBlue));
         listView.setAdapter(listAdapter);
         listView.setItemAnimator(null);
         listView.setLayoutAnimation(null);
+
         frameLayout.addView(listView, LayoutHelper.createFrame(LayoutHelper.MATCH_PARENT, LayoutHelper.MATCH_PARENT, Gravity.TOP | Gravity.LEFT));
+
         listView.setOnItemClickListener((view, position, x, y) -> {
-            if (position == inappCameraRow) {
+            if (position == inappCameraRow && view instanceof TextCheckCell) {
                 SharedConfig.toggleInappCamera();
-                if (view instanceof TextCheckCell) {
-                    ((TextCheckCell) view).setChecked(SharedConfig.inappCamera);
-                }
+                ((TextCheckCell) view).setChecked(SharedConfig.inappCamera);
             }
         });
-
-        return fragmentView;
     }
 
     @Override
@@ -114,10 +103,9 @@ public class ForkSettingsActivity extends BaseFragment {
     }
 
     private class ListAdapter extends RecyclerListView.SelectionAdapter {
+        private final Context mContext;
 
-        private Context mContext;
-
-        public ListAdapter(Context context) {
+        ListAdapter(Context context) {
             mContext = context;
         }
 
@@ -127,85 +115,77 @@ public class ForkSettingsActivity extends BaseFragment {
         }
 
         @Override
+        public int getItemViewType(int position) {
+            if (position == inappCameraRow) {
+                return 3; // TextCheckCell
+            } else if (position == sectionRow1) {
+                return 4; // HeaderCell
+            }
+            return 6; // Default type for other cases
+        }
+
+        @Override
+        public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = createViewForType(viewType);
+            view.setLayoutParams(new RecyclerView.LayoutParams(RecyclerView.LayoutParams.MATCH_PARENT, RecyclerView.LayoutParams.WRAP_CONTENT));
+            return new RecyclerListView.Holder(view);
+        }
+
+        private View createViewForType(int viewType) {
+            switch (viewType) {
+                case 1:
+                    return new ShadowSectionCell(mContext);
+                case 3:
+                    return createTextCheckCell();
+                case 4:
+                    return createHeaderCell();
+                default:
+                    return new View(mContext); // Placeholder for default cases
+            }
+        }
+
+        private View createTextCheckCell() {
+            TextCheckCell textCell = new TextCheckCell(mContext);
+            textCell.setBackgroundColor(Theme.getColor(Theme.key_windowBackgroundWhite));
+            return textCell;
+        }
+
+        private View createHeaderCell() {
+            HeaderCell headerCell = new HeaderCell(mContext);
+            headerCell.setBackgroundColor(Theme.getColor(Theme.key_windowBackgroundWhite));
+            return headerCell;
+        }
+
+        @Override
         public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
-            switch (holder.getItemViewType()) {
-                case 2: {
-                    TextSettingsCell textCell = (TextSettingsCell) holder.itemView;
-                    break;
-                }
-                case 3: {
-                    TextCheckCell textCell = (TextCheckCell) holder.itemView;
-                    SharedPreferences preferences = MessagesController.getGlobalMainSettings();
-                    if (position == inappCameraRow) {
-                        String t = LocaleController.getString("InAppCamera", R.string.InAppCamera);
-                        String info = LocaleController.getString("InAppCameraInfo", R.string.InAppCameraInfo);
-                        textCell.setTextAndValueAndCheck(t, info, preferences.getBoolean("inappCamera", true), false, false);
-                    }
-                    break;
-                }
-                case 4: {
-                    HeaderCell headerCell = (HeaderCell) holder.itemView;
-                    if (position == sectionRow1) {
-                        headerCell.setText(LocaleController.getString("General", R.string.General));
-                    }
-                    break;
-                }
+            if (holder.getItemViewType() == 3) { // TextCheckCell
+                bindTextCheckCell(holder, position);
+            } else if (holder.getItemViewType() == 4) { // HeaderCell
+                bindHeaderCell(holder, position);
+            }
+        }
+
+        private void bindTextCheckCell(RecyclerView.ViewHolder holder, int position) {
+            TextCheckCell textCell = (TextCheckCell) holder.itemView;
+            SharedPreferences preferences = MessagesController.getGlobalMainSettings();
+            if (position == inappCameraRow) {
+                String title = LocaleController.getString("InAppCamera", R.string.InAppCamera);
+                String info = LocaleController.getString("InAppCameraInfo", R.string.InAppCameraInfo);
+                textCell.setTextAndValueAndCheck(title, info, preferences.getBoolean("inappCamera", true), false, false);
+            }
+        }
+
+        private void bindHeaderCell(RecyclerView.ViewHolder holder, int position) {
+            HeaderCell headerCell = (HeaderCell) holder.itemView;
+            if (position == sectionRow1) {
+                headerCell.setText(LocaleController.getString("General", R.string.General));
             }
         }
 
         @Override
         public boolean isEnabled(RecyclerView.ViewHolder holder) {
             int position = holder.getAdapterPosition();
-            boolean fork = false
-                        || position == inappCameraRow;
-            return fork;
-        }
-
-        @Override
-        public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            View view = null;
-            switch (viewType) {
-                case 1:
-                    view = new ShadowSectionCell(mContext);
-                    break;
-                case 2:
-                    view = new TextSettingsCell(mContext);
-                    view.setBackgroundColor(Theme.getColor(Theme.key_windowBackgroundWhite));
-                    break;
-                case 3:
-                    view = new TextCheckCell(mContext);
-                    view.setBackgroundColor(Theme.getColor(Theme.key_windowBackgroundWhite));
-                    break;
-                case 4:
-                    view = new HeaderCell(mContext);
-                    view.setBackgroundColor(Theme.getColor(Theme.key_windowBackgroundWhite));
-                    break;
-                case 5:
-                    view = new NotificationsCheckCell(mContext);
-                    view.setBackgroundColor(Theme.getColor(Theme.key_windowBackgroundWhite));
-                    break;
-                case 6:
-                    view = new TextDetailSettingsCell(mContext);
-                    view.setBackgroundColor(Theme.getColor(Theme.key_windowBackgroundWhite));
-                    break;
-            }
-            view.setLayoutParams(new RecyclerView.LayoutParams(RecyclerView.LayoutParams.MATCH_PARENT, RecyclerView.LayoutParams.WRAP_CONTENT));
-            return new RecyclerListView.Holder(view);
-        }
-
-        @Override
-        public int getItemViewType(int position) {
-            if (position == emptyRow) {
-                return 1;
-            } else if (0 == 1) {
-                return 2;
-            } else if (false
-                || position == inappCameraRow) {
-                return 3;
-            } else if (position == sectionRow1) {
-                return 4;
-            }
-            return 6;
+            return position == inappCameraRow;
         }
     }
 
@@ -213,41 +193,50 @@ public class ForkSettingsActivity extends BaseFragment {
     public ArrayList<ThemeDescription> getThemeDescriptions() {
         ArrayList<ThemeDescription> themeDescriptions = new ArrayList<>();
 
-        themeDescriptions.add(new ThemeDescription(listView, ThemeDescription.FLAG_CELLBACKGROUNDCOLOR, new Class[]{EmptyCell.class, TextSettingsCell.class, TextCheckCell.class, HeaderCell.class, TextDetailSettingsCell.class, NotificationsCheckCell.class}, null, null, null, Theme.key_windowBackgroundWhite));
+        themeDescriptions.add(new ThemeDescription(listView, ThemeDescription.FLAG_CELLBACKGROUNDCOLOR, new Class[]{TextCheckCell.class, HeaderCell.class}, null, null, null, Theme.key_windowBackgroundWhite));
         themeDescriptions.add(new ThemeDescription(fragmentView, ThemeDescription.FLAG_BACKGROUND, null, null, null, null, Theme.key_windowBackgroundGray));
 
-        themeDescriptions.add(new ThemeDescription(actionBar, ThemeDescription.FLAG_BACKGROUND, null, null, null, null, Theme.key_avatar_backgroundActionBarBlue));
-        themeDescriptions.add(new ThemeDescription(listView, ThemeDescription.FLAG_LISTGLOWCOLOR, null, null, null, null, Theme.key_avatar_backgroundActionBarBlue));
-        themeDescriptions.add(new ThemeDescription(actionBar, ThemeDescription.FLAG_AB_ITEMSCOLOR, null, null, null, null, Theme.key_avatar_actionBarIconBlue));
-        themeDescriptions.add(new ThemeDescription(actionBar, ThemeDescription.FLAG_AB_TITLECOLOR, null, null, null, null, Theme.key_actionBarDefaultTitle));
-        themeDescriptions.add(new ThemeDescription(actionBar, ThemeDescription.FLAG_AB_SELECTORCOLOR, null, null, null, null, Theme.key_avatar_actionBarSelectorBlue));
-        themeDescriptions.add(new ThemeDescription(actionBar, ThemeDescription.FLAG_AB_SUBMENUBACKGROUND, null, null, null, null, Theme.key_actionBarDefaultSubmenuBackground));
-        themeDescriptions.add(new ThemeDescription(actionBar, ThemeDescription.FLAG_AB_SUBMENUITEM, null, null, null, null, Theme.key_actionBarDefaultSubmenuItem));
-
-        themeDescriptions.add(new ThemeDescription(listView, ThemeDescription.FLAG_SELECTOR, null, null, null, null, Theme.key_listSelector));
-
-        themeDescriptions.add(new ThemeDescription(listView, 0, new Class[]{View.class}, Theme.dividerPaint, null, null, Theme.key_divider));
-
-        themeDescriptions.add(new ThemeDescription(listView, ThemeDescription.FLAG_BACKGROUNDFILTER, new Class[]{ShadowSectionCell.class}, null, null, null, Theme.key_windowBackgroundGrayShadow));
-
-        themeDescriptions.add(new ThemeDescription(listView, 0, new Class[]{TextSettingsCell.class}, new String[]{"textView"}, null, null, null, Theme.key_windowBackgroundWhiteBlackText));
-        themeDescriptions.add(new ThemeDescription(listView, 0, new Class[]{TextSettingsCell.class}, new String[]{"valueTextView"}, null, null, null, Theme.key_windowBackgroundWhiteValueText));
-
-        themeDescriptions.add(new ThemeDescription(listView, 0, new Class[]{NotificationsCheckCell.class}, new String[]{"textView"}, null, null, null, Theme.key_windowBackgroundWhiteBlackText));
-        themeDescriptions.add(new ThemeDescription(listView, 0, new Class[]{NotificationsCheckCell.class}, new String[]{"valueTextView"}, null, null, null, Theme.key_windowBackgroundWhiteGrayText2));
-        themeDescriptions.add(new ThemeDescription(listView, 0, new Class[]{NotificationsCheckCell.class}, new String[]{"checkBox"}, null, null, null, Theme.key_switchTrack));
-        themeDescriptions.add(new ThemeDescription(listView, 0, new Class[]{NotificationsCheckCell.class}, new String[]{"checkBox"}, null, null, null, Theme.key_switchTrackChecked));
-
-        themeDescriptions.add(new ThemeDescription(listView, 0, new Class[]{TextCheckCell.class}, new String[]{"textView"}, null, null, null, Theme.key_windowBackgroundWhiteBlackText));
-        themeDescriptions.add(new ThemeDescription(listView, 0, new Class[]{TextCheckCell.class}, new String[]{"valueTextView"}, null, null, null, Theme.key_windowBackgroundWhiteGrayText2));
-        themeDescriptions.add(new ThemeDescription(listView, 0, new Class[]{TextCheckCell.class}, new String[]{"checkBox"}, null, null, null, Theme.key_switchTrack));
-        themeDescriptions.add(new ThemeDescription(listView, 0, new Class[]{TextCheckCell.class}, new String[]{"checkBox"}, null, null, null, Theme.key_switchTrackChecked));
-
-        themeDescriptions.add(new ThemeDescription(listView, 0, new Class[]{HeaderCell.class}, new String[]{"textView"}, null, null, null, Theme.key_windowBackgroundWhiteBlueHeader));
-
-        themeDescriptions.add(new ThemeDescription(listView, 0, new Class[]{TextDetailSettingsCell.class}, new String[]{"textView"}, null, null, null, Theme.key_windowBackgroundWhiteBlackText));
-        themeDescriptions.add(new ThemeDescription(listView, 0, new Class[]{TextDetailSettingsCell.class}, new String[]{"valueTextView"}, null, null, null, Theme.key_windowBackgroundWhiteGrayText2));
+        themeDescriptions.addAll(getActionBarThemeDescriptions());
+        themeDescriptions.addAll(getListViewThemeDescriptions());
 
         return themeDescriptions;
+    }
+
+    private ArrayList<ThemeDescription> getActionBarThemeDescriptions() {
+        ArrayList<ThemeDescription> descriptions = new ArrayList<>();
+
+        descriptions.add(new ThemeDescription(actionBar, ThemeDescription.FLAG_BACKGROUND, null, null, null, null, Theme.key_avatar_backgroundActionBarBlue));
+        descriptions.add(new ThemeDescription(actionBar, ThemeDescription.FLAG_AB_ITEMSCOLOR, null, null, null, null, Theme.key_avatar_actionBarIconBlue));
+        descriptions.add(new ThemeDescription(actionBar, ThemeDescription.FLAG_AB_TITLECOLOR, null, null, null, null, Theme.key_actionBarDefaultTitle));
+        descriptions.add(new ThemeDescription(actionBar, ThemeDescription.FLAG_AB_SELECTORCOLOR, null, null, null, null, Theme.key_avatar_actionBarSelectorBlue));
+        descriptions.add(new ThemeDescription(actionBar, ThemeDescription.FLAG_AB_SUBMENUBACKGROUND, null, null, null, null, Theme.key_actionBarDefaultSubmenuBackground));
+        descriptions.add(new ThemeDescription(actionBar, ThemeDescription.FLAG_AB_SUBMENUITEM, null, null, null, null, Theme.key_actionBarDefaultSubmenuItem));
+
+        return descriptions;
+    }
+
+    private ArrayList<ThemeDescription> getListViewThemeDescriptions() {
+        ArrayList<ThemeDescription> descriptions = new ArrayList<>();
+
+        descriptions.add(new ThemeDescription(listView, ThemeDescription.FLAG_LISTGLOWCOLOR, null, null, null, null, Theme.key_avatar_backgroundActionBarBlue));
+        descriptions.add(new ThemeDescription(listView, ThemeDescription.FLAG_SELECTOR, null, null, null, null, Theme.key_listSelector));
+        descriptions.add(new ThemeDescription(listView, ThemeDescription.FLAG_BACKGROUNDFILTER, new Class[]{ShadowSectionCell.class}, null, null, null, Theme.key_windowBackgroundGrayShadow));
+
+        descriptions.addAll(getCellTextColors());
+
+        return descriptions;
+    }
+
+    private ArrayList<ThemeDescription> getCellTextColors() {
+        ArrayList<ThemeDescription> descriptions = new ArrayList<>();
+
+        descriptions.add(new ThemeDescription(listView, 0, new Class[]{TextCheckCell.class}, new String[]{"textView"}, null, null, null, Theme.key_windowBackgroundWhiteBlackText));
+        descriptions.add(new ThemeDescription(listView, 0, new Class[]{TextCheckCell.class}, new String[]{"valueTextView"}, null, null, null, Theme.key_windowBackgroundWhiteGrayText2));
+        descriptions.add(new ThemeDescription(listView, 0, new Class[]{TextCheckCell.class}, new String[]{"checkBox"}, null, null, null, Theme.key_switchTrack));
+        descriptions.add(new ThemeDescription(listView, 0, new Class[]{TextCheckCell.class}, new String[]{"checkBox"}, null, null, null, Theme.key_switchTrackChecked));
+
+        descriptions.add(new ThemeDescription(listView, 0, new Class[]{HeaderCell.class}, new String[]{"textView"}, null, null, null, Theme.key_windowBackgroundWhiteBlueHeader));
+
+        return descriptions;
     }
 }
